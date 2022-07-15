d35e54847596eb4845e31e301b05227f0163930c
juliusjessestose <estosejuliusjess@gmail.com>
2022-07-15 10:13:25 +0800

    first commit

---
+++ src/stores/auth.js
@@ -0,0 +1,19 @@
+import { defineStore } from "pinia";
+export const useAuthStore = defineStore({
+  id: "counter",
+  persist:true,
+  state: () => ({
+    user: {},
+    token: "",
+  }),
+  actions: {
+    saveAuth(user, token) {
+      this.user = user;
+      this.token = token;
+    },
+    destroy() {
+      this.user = {};
+      this.token = "";
+    },
+  },
+});
